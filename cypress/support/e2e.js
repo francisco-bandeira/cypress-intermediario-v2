5fb8159bee32a231d1d1ee8292863a02fde1598c
Francisco Bandeira <francisco.frazzon@outlook.com>
2024-05-16 19:28:57 -0300

    criando cli

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import 'cypress-plugin-api'
 import './api_commands'
+import './cli_commands'
 import './gui_commands'
 
 // Alternatively you can use CommonJS syntax:
